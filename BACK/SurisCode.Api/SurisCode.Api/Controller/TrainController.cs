eff0c6007e8770ea0f6d65203a5556aa6d354bf0
Mariano Passerini <renacer258@gmail.com>
2024-05-14 18:26:03 -0300

    first commit

---
+++ BACK/SurisCode.Api/SurisCode.Api/Controller/TrainController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurisCode.Api.Model;
+
+namespace SurisCode.Api.Controller
+{
+    [Controller]
+    [Route("api/train")]
+    public class TrainController : ControllerBase
+    {
+        public static List<int> Tren = new List<int>();
+        public TrainController() { }
+
+        [HttpPost]
+        [Route("/Add")]
+        public IActionResult Add([FromBody]TrainParams vagon)
+        {
+            if (vagon.Right)
+            {
+                Tren.Add(vagon.Vagon);
+            } else
+            {
+                Tren.Insert(0, vagon.Vagon);
+            }
+            return Ok(Tren);
+        }
+
+        [HttpPost]
+        [Route("/Delete")]
+        public IActionResult Remove([FromBody]bool right)
+        {
+            if (right)
+            {
+                Tren.RemoveAt(Tren.Count - 1);
+            }
+            else
+            {
+                Tren.RemoveAt(0);
+            }
+
+            return Ok(Tren);
+        }
+
+    }
+}
---
+++ BACK/SurisCode.Api/SurisCode.Api/Model/TrainParams.cs
@@ -0,0 +1,8 @@
+﻿namespace SurisCode.Api.Model
+{
+    public class TrainParams
+    {
+       public int Vagon {  get; set; }
+       public bool Right { get; set; }
+    }
+}
